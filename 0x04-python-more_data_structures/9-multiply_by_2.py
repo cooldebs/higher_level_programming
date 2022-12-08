1e919ce58de474fc9dbb3ef8bf19e1ab9cd49c46
cooldebs <doduronbi83@gmail.com>
2022-12-07 16:35:03 -0800

    Write a function that returns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for value in a_dictionary:
-        return [list(map(lambda x: x * 2, value))]
+    new_dictionary = a_dictionary.copy()
+    for value in new_dictionary.keys():
+        new_dictionary[i] *= 2
+    return new_dictionary
