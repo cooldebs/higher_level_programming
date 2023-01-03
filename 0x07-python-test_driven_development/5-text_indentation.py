13580652ea190b4077cc3542223ac54184d091a3
cooldebs <doduronbi83@gmail.com>
2023-01-03 12:59:04 -0800

    Hot fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-"""The "5-text_indentation" module
-
-The module defines a text-indentation function.
+"""The "5-text_indentation module
+Function:  text_indentation(text)
 """
 
 
 def text_indentation(text):
-    """ Prints a text with two new lines after each: ., ? and :
+    """ prints a text with 2 new lines after \
+    each of these characters: ., ? and :
     """
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
+    text_list = list(text)
+    text_list.append("xz")
     i = 0
-    while i < len(text) and text[i] == ' ':
+
+    while text_list[i] is not "xz":
+        if text_list[i] in ['.', '?', ':']:
+            while text_list[i + 1] == " ":
+                text_list.pop(i + 1)
+            while text_list[i - 1] == " ":
+                text_list.pop(i - 1)
+                i -= 1
+            text_list.insert(i + 1, '\n')
+            text_list.insert(i + 2, '\n')
+            i += 1
         i += 1
 
-    while i < len(text):
-        print(text[i]. end="")
-        if text[i] == "\n" or text[i] in ".?:":
-            if text[i] in ".?:":
-                print("\n")
-            i += 1
-            while i < len(text) and text[i] == ' ':
-                i += 1
-            continue
-    i += 1
+    text_list.pop()
+    for i in text_list:
+        print(i, end="")
