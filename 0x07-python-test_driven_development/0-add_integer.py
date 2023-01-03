7d252377c36db5bd549d14d821a745a5a86b89da
cooldebs <doduronbi83@gmail.com>
2023-01-03 04:40:22 -0800

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 The "0-add_integer" module.
-
-The module defines an integer addition function.
+The module contains one function "add_integer"
 """
 
 
@@ -10,8 +9,10 @@ def add_integer(a, b=98):
     """
     Returns a + b
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    x = [float("inf"), float("-inf")]
+    if (type(a) is not int and type(a) is not float) \
+       or a in x or a != a:
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if (type(b) is not int and type(b) is not float) \
+       or b in x or b != b:
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
