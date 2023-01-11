af05ac459ecc324b9ac0bea52a994cb3eef6cfb0
cooldebs <doduronbi83@gmail.com>
2023-01-11 13:24:12 -0800

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module "9-student"
+"""
+
+
+class Student:
+    """ Defines a student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
