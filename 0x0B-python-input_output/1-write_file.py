70a84558712ee4a7a0527d8e90d52dff84afdc9b
cooldebs <doduronbi83@gmail.com>
2023-01-11 12:25:01 -0800

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module "1-write_file"
+"""
+
+
+def write_file(filename="", text=""):
+    """ writes a string to a text file (UTF8) and
+    returns the number of characters written
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
+        return f.seek(0, 2)
