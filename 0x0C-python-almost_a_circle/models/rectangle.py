903f8cf88157eca0db13657c4203d937bedea886
cooldebs <doduronbi83@gmail.com>
2023-01-14 18:01:44 -0800

    Task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,46 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
+        self.__y = value
 
     def area(self):
         """ Returns the area of a rectangle """
         return self.height * self.width
+
+    def display(self):
+        """ prints in stdout the Rectangle instance with
+        the character #
+        """
+        for pad_y in range(0, self.y):
+            print()
+        for row in range(0, self.height):
+            for pad_x in range(0, self.x):
+                print(end=" ")
+            for col in range(0, self.width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """ prints a user friendly string """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """ assigns arguments to each attribute """
+        index = 0
+        if args is not None and len(args) != 0:
+            for argument in args:
+                index += 1
+                if index == 1:
+                    self.id = argument
+                if index == 2:
+                    self.__width = argument
+                if index == 3:
+                    self.__height = argument
+                if index == 4:
+                    self.__x = argument
+                if index == 5:
+                    self.__y = argument
+        else:
+            for key, values in kwargs.items():
+                setattr(self, key, values)
