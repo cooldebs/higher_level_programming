8ea2de950f2d3e838b1db7b3e68bb52494d40899
cooldebs <doduronbi83@gmail.com>
2023-03-24 21:47:30 -0700

    Sql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,8 +10,6 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id""")
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
-                INNER JOIN states ON states.id=cities.state_id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
